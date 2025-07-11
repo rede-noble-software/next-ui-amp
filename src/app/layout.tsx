18c1c7cd12d5933c24f472ac06a2b476f545e2e1
ribonred <ribonred@gmail.com>
2025-07-11 18:22:41 +0700

    some change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Next.js UI App',
-  description: 'A simple Next.js application ready for AWS Amplify deployment - changes 1',
+  description: 'A simple Next.js application ready for AWS Amplify deployment - changes 1.x3',
 }
 
 export default function RootLayout({
